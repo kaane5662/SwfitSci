5ba7374da1a3bcfde647eacd16f153f4e7c9ca03
kaane5662 <kaane0169@gmail.com>
2024-06-25 00:28:38 -0400

    implemented custom prompts, rewrote backend to handle custom prompts

--- client/swiftsci/app/components/Generate/Figure.tsx
+++
@@ -1,9 +0,0 @@
-// import { Figure } from "@/app/types";
-
-// export default function Figure({name,data}:Figure){
-//     return(
-//         <div className="flex flex-col gap-4">
-//             <textarea wrap="whitespace" className="h-[200px] p-2 rounded-md w-[100%]" placeholder={`Dogs vs Cats \n Regression Model \n r=.5 \n r^2=.5 \n p=.021 `}></textarea>
-//         </div>
-//     )
-// }
